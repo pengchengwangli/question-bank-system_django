ef64d5057cc7422feb257a8b557833fabac059e2
Peng_root <3347132783@qq.com>
2024-06-16 20:41:11 +0800

    添加缺少配置

--- question_bank_system_django/settings.py
+++ question_bank_system_django/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
+    'ckeditor5',
     'ckeditor_uploader'
 ]
 
